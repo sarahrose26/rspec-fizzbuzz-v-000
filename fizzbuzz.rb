64e4f6e40315eea610a49c73d74d63a893ff2db4
Sarah Dahnke <sarahrosedahnke@gmail.com>
2017-09-08 02:15:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 && int % 5
+  elsif int % 3 && int % 5 == 0
     return "FizzBuzz"
   else
-    return nil 
+    return nil
   end
 end
